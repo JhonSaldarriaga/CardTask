25c6f252b78d2b6b82394a478be94f8b816a400c
Jhon Saldarriaga <jhonsaldarriagalopez@gmail.com>
2021-06-02 21:22:29 -0500

    init project

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Date;
+
+public class Task {
+
+    private int id;
+    private String title;
+    private String description;
+    private Date data;
+
+    
+}
