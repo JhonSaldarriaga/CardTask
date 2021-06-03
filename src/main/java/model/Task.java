b2a75edc75df0ab58e168ea96da0f2ca03fcbd76
Jhon Saldarriaga <jhonsaldarriagalopez@gmail.com>
2021-06-02 21:53:50 -0500

    Model and provider has been implemented

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,13 +1,74 @@
 package model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Task {
 
+    public static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
     private int id;
     private String title;
     private String description;
-    private Date data;
+    private String category;
+    private Date date;
 
-    
+    public Task(int id, String title, String description, String category, Date date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.date = date;
+    }
+
+    public Task(String title, String description, String category, Date date) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public static SimpleDateFormat getFormat() {
+        return format;
+    }
+
+    public static void setFormat(SimpleDateFormat format) {
+        Task.format = format;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
--- src/main/java/provider/TaskProvider.java
+++ src/main/java/provider/TaskProvider.java
@@ -1,4 +1,82 @@
 package provider;
 
+import db.DBConnection;
+import model.Task;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class TaskProvider {
+
+    public void addTask(Task task) throws SQLException {
+        DBConnection connection = new DBConnection();
+        String date = Task.format.format(task.getDate());
+        String sql = ("INSERT INTO A00362210_tasks (title, description, category, date) " +
+                "VALUES ($TITLE,$DESCRIPTION,$CATEGORY,$DATE)")
+                .replace("$TITLE","'"+task.getTitle()+"'")
+                .replace("$DESCRIPTION", "'"+task.getDescription()+"'")
+                .replace("$CATEGORY","'"+task.getCategory()+"'")
+                .replace("$DATE","'" + date  + "'");
+        connection.connect();
+        connection.commandSQL(sql);
+        connection.disconnect();
+    }
+
+    public void updateTask(Task task) throws SQLException {
+        DBConnection connection = new DBConnection();
+        String date =Task.format.format(task.getDate());
+        String sql = ("UPDATE A00362210_tasks SET title=$TITLE, description=$DESCRIPTION, category=$CATEGORY, date=$DATE")
+                .replace("$TITLE","'"+task.getTitle()+"'")
+                .replace("$DESCRIPTION", "'"+task.getDescription()+"'")
+                .replace("$CATEGORY","'"+task.getCategory()+"'")
+                .replace("$DATE","'" + date  + "'");
+        connection.connect();
+        connection.commandSQL(sql);
+        connection.disconnect();
+    }
+
+    public void deleteTask(int id) throws SQLException {
+        DBConnection connection = new DBConnection();
+        String sql = ("DELETE FROM A00362210_tasks WHERE id=$ID").replace("$ID", ""+id);
+        connection.connect();
+        connection.commandSQL(sql);
+        connection.disconnect();
+    }
+
+    private Task getTask(int id) throws SQLException, ParseException {
+        String sql = "SELECT * FROM A00362210_tasks WHERE id=$ID".replace("$ID",""+id);
+        DBConnection connection = new DBConnection();
+        connection.connect();
+        ResultSet resultSet =  connection.getDataBySQL(sql);
+        Task task = null;
+        if(resultSet.next()) {
+            String title = resultSet.getString(resultSet.findColumn("title"));
+            String description = resultSet.getString(resultSet.findColumn("description"));
+            String category = resultSet.getString(resultSet.findColumn("category"));
+            String date = resultSet.getString(resultSet.findColumn("date"));
+            task = new Task(id,title,description,category, Task.format.parse(date));
+        }
+        connection.disconnect();
+        return task;
+    }
+
+    private ArrayList<Task> getAllTasks() throws SQLException, ParseException {
+        String sql = "SELECT * FROM A00362210_tasks";
+        ArrayList<Task> tasks = new ArrayList<>();
+        DBConnection connection = new DBConnection();
+        connection.connect();
+        ResultSet resultSet =  connection.getDataBySQL(sql);
+        while(resultSet.next()) {
+            int id = Integer.parseInt(resultSet.getString(resultSet.findColumn("id")));
+            String title = resultSet.getString(resultSet.findColumn("title"));
+            String description = resultSet.getString(resultSet.findColumn("description"));
+            String category = resultSet.getString(resultSet.findColumn("category"));
+            String date = resultSet.getString(resultSet.findColumn("date"));
+            tasks.add(new Task(id,title,description,category, Task.format.parse(date)));
+        }
+        connection.disconnect();
+        return tasks;
+    }
 }
