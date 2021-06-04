fd1187dfb4d86393edf482b3db1883c74bbed5d8
Jhon Saldarriaga <jhonsaldarriagalopez@gmail.com>
2021-06-03 22:11:49 -0500

    little change in index.js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -5,6 +5,11 @@ const description = document.getElementById('descriptionTask');
 addTask = () => {
     const task = new Task(title.value, description.value, "to_do", "2021-06-03");
     let xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', ()=>{
+        if(xhr.readyState ===4 ){
+            console.log(xhr.responseText);
+        }
+    })
     xhr.open("POST", "http://localhost:8081/CardTask_war/api/task/add");
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify(task));
